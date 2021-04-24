78aa1e4db73f5f6271b9b139981f5f82dd7a579c
rwxrob <rob@rwx.gg>
2021-04-24 19:29:56 -0400

    Simplify doc.go

--- util/doc.go
+++ util/doc.go
@@ -1,6 +1,6 @@
 /*
-Package util contains utilities that are used by CmdBox itself or that
-are frequently needed and useful when writing command Method
-implementations (and are not already covered by other standard stuff).
+Package util contains utilities used by CmdBox itself or that
+are frequently needed and useful when writing Command.Method
+implementations.
 */
 package util
