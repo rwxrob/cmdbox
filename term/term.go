abb225a229ac4f85da3890f28e4d8470c00d263f
rwxrob <rob@rwx.gg>
2021-05-30 14:14:14 -0400

    Factor isterm into cmdbox-isterm (close #48)

--- term/term.go
+++ term/term.go
@@ -17,7 +17,8 @@ var WinSize = &struct {
 // IsTerminal returns true if the output is to an interactive terminal
 // (not piped in any way). This is useful when detemining if an extra
 // line return is needed to avoid making programs chomp the line returns
-// unnecessarily.
+// unnecessarily. (See github.com/rwxrob/cmdbox-isterm for utility that
+// uses this function.)
 func IsTerminal() bool {
 	if fileInfo, _ := os.Stdout.Stat(); (fileInfo.Mode() & os.ModeCharDevice) != 0 {
 		return true
