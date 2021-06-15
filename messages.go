199c5e838fc52e66720572e600a38533f4b1652e
rwxrob <rob@rwx.gg>
2021-06-15 00:49:38 -0400

    Add new.go to own file, big enough documentation

---
+++ messages.go
@@ -0,0 +1,8 @@
+package cmdbox
+
+// Keep all messages in this file so they can be overriden by composite
+// authors depending on the locale.
+
+var Messages = map[string]string{
+	"InvalidName": "invalid name (lower case words only)",
+}
