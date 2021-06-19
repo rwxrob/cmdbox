5331d79bc81077bd9807ee154c4f78794593c9ee
rwxrob <rob@rwx.gg>
2021-06-18 21:13:31 -0400

    Unify ToJSON and ToYAML in util package

--- util/jsonyaml.go
+++ util/jsonyaml.go
@@ -1,10 +1,22 @@
 package util
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"gopkg.in/yaml.v3"
 )
+
+// ToJSON converts any object to its compact JSON string
+// equivalent.  If an error is encountered while marshalling an ERROR
+// key will be created with the string value of the error as its value.
+func ToJSON(a interface{}) string {
+	byt, err := json.Marshal(a)
+	if err != nil {
+		return fmt.Sprintf("{\"ERROR\": \"%v\"}", err)
+	}
+	return string(byt)
+}
 
 // ToYAML converts any object to its compact YAML string equivalent.  If
 // an error is encountered while marshalling an ERROR key will be
--- util/jsonyaml_test.go
+++ util/jsonyaml_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/rwxrob/cmdbox/util"
 )
 
-func ExampleConvertToPrettyJSON() {
+func ExampleToJSON() {
 
 	sample := map[string]interface{}{}
 	sample["int"] = 1
@@ -15,35 +15,31 @@ func ExampleConvertToPrettyJSON() {
 	sample["map"] = map[string]interface{}{"blah": "another"}
 	sample["array"] = []string{"blah", "another"}
 
-	fmt.Println(util.ConvertToPrettyJSON(sample))
-
-	// Unordered output:
-	//
-	// {
-	//   "array": [
-	//     "blah",
-	//     "another"
-	//   ],
-	//   "float": 1,
-	//   "int": 1,
-	//   "map": {
-	//     "blah": "another"
-	//   },
-	//   "string": "some thing"
-	// }
-}
-
-func ExampleConvertToJSON() {
-
-	sample := map[string]interface{}{}
-	sample["int"] = 1
-	sample["float"] = 1
-	sample["string"] = "some thing"
-	sample["map"] = map[string]interface{}{"blah": "another"}
-	sample["array"] = []string{"blah", "another"}
-
-	fmt.Println(util.ConvertToJSON(sample))
+	fmt.Println(util.ToJSON(sample))
 
 	// Output:
 	// {"array":["blah","another"],"float":1,"int":1,"map":{"blah":"another"},"string":"some thing"}
+}
+
+func ExampleToYAML() {
+
+	sample := map[string]interface{}{}
+	sample["int"] = 1
+	sample["float"] = 1
+	sample["string"] = "some thing"
+	sample["map"] = map[string]interface{}{"blah": "another"}
+	sample["array"] = []string{"blah", "another"}
+
+	fmt.Println(util.ToYAML(sample))
+
+	// Unordered output:
+	// array:
+	//     - blah
+	//     - another
+	// float: 1
+	// int: 1
+	// map:
+	//     blah: another
+	// string: some thing
+
 }
