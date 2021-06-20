e8dddd96b89105b8fc6f2e33faebcbe9db6c7160
rwxrob <rob@rwx.gg>
2021-06-20 17:57:39 -0400

    Add Apache license preamble (closes #77)
    Apparently some legal teams still prefer this to be included in every
    file containing code.

--- valid/doc.go
+++
@@ -1,4 +0,0 @@
-/* The valid package simply and efficiently minimally validates command
- * line components passed into CmdBox commands, aliases, and such.
- */
-package valid
--- valid/valid.go
+++ valid/valid.go
@@ -1,3 +1,6 @@
+/* The valid package simply and efficiently minimally validates command
+ * line components passed into CmdBox commands, aliases, and such.
+ */
 package valid
 
 import (
