2e38e9d4f422f37a8813b0c99bbad1cee66e0ce9
rwxrob <rob@rwx.gg>
2021-06-28 17:56:33 -0400

    Factor Map into util.StringMap for reuse

---
+++ util/stringmap.go
@@ -0,0 +1,258 @@
+/*
+Copyright 2021 Robert S. Muhlestein.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+	"sort"
+	"strings"
+	"sync"
+)
+
+// StringMap is a high-level type used to contain string data for both keys
+// and values Developers can use the StringMap type in their own modules as
+// a convenience since requires less declaration and is 100% safe for
+// concurrency (embeds synx.Mutex). The internal map is exported (as M) for
+// when developers want to do their own locking and mutations rather
+// than use the public interface methods.
+type StringMap struct {
+	M map[string]string
+	sync.Mutex
+}
+
+// NewStringMap returns a new StringMap with the internal
+// map[string]string initialized.
+func NewStringMap() *StringMap {
+	m := new(StringMap)
+	m.Init()
+	return m
+}
+
+// ToStringMap converts a map[string]interface{} into a StringMap
+// creating all new map values (concurrency safe clone).
+func ToStringMap(m map[string]interface{}) *StringMap {
+	n := NewStringMap()
+	defer n.Unlock()
+	n.Lock()
+	for k, v := range m {
+		n.M[k] = v.(string)
+	}
+	return n
+}
+
+// Init initializes (or re-initialized) the StringMap deleting all its
+// values (without changing its reference).
+func (m *StringMap) Init() {
+	defer m.Unlock()
+	m.Lock()
+	if m.M == nil {
+		m.M = make(map[string]string)
+		return
+	}
+	for k := range m.M {
+		delete(m.M, k)
+	}
+}
+
+// Get returns a value by key name safe for concurrency. Returns empty
+// string if not found.
+func (m *StringMap) Get(key string) string {
+	defer m.Unlock()
+	m.Lock()
+	if v, has := m.M[key]; has {
+		return v
+	}
+	return ""
+}
+
+// Set set a value by key name safe in a way that is for concurrency.
+func (m *StringMap) Set(key, val string) {
+	defer m.Unlock()
+	m.Lock()
+	m.M[key] = val
+}
+
+// Delete removes one or more entries from the map in a way that is safe
+// for concurrency.
+func (m *StringMap) Delete(keys ...string) {
+	defer m.Unlock()
+	m.Lock()
+	for _, k := range keys {
+		delete(m.M, k)
+	}
+}
+
+// Rename changes a key name in a manner safe for concurrency by adding
+// the new name with the same value and deleting the old one.
+func (m *StringMap) Rename(from, to string) {
+	defer m.Unlock()
+	m.Lock()
+	v, has := m.M[from]
+	if !has {
+		return
+	}
+	m.M[to] = v
+	delete(m.M, from)
+}
+
+// ------------------------------ queries -----------------------------
+
+// Same returns a sorted list of all keys that are also values.
+func (m StringMap) Same() []string {
+	defer m.Unlock()
+	m.Lock()
+	same := []string{}
+	for k, v := range m.M {
+		if k == v {
+			same = append(same, v)
+		}
+	}
+	sort.Strings(same)
+	return same
+}
+
+// Keys returns a sorted list of all possible string keys.
+func (m StringMap) Keys() []string {
+	defer m.Unlock()
+	m.Lock()
+	keys := make([]string, len(m.M))
+	var i int
+	for k, _ := range m.M {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+// Values returns a sorted list of all unique values safe for
+// concurrency.
+func (m StringMap) Values() []string {
+	defer m.Unlock()
+	m.Lock()
+	vals := []string{}
+	seen := map[string]bool{}
+	for _, v := range m.M {
+		if _, saw := seen[v]; saw {
+			continue
+		}
+		vals = append(vals, v)
+		seen[v] = true
+	}
+	sort.Strings(vals)
+	return vals
+}
+
+// Aliases returns only the keys that are not identical to their value.
+func (m StringMap) Aliases() []string {
+	defer m.Unlock()
+	m.Lock()
+	a := []string{}
+	for k, v := range m.M {
+		if k == v {
+			continue
+		}
+		a = append(a, k)
+	}
+	sort.Strings(a)
+	return a
+}
+
+// Slice returns a slice of values fetched from the StringMap in order
+// that match the key names passed. If a name is not found its value
+// will be an empty string. Slice is safe for concurrency.
+func (m StringMap) Slice(keys ...string) []string {
+	defer m.Unlock()
+	m.Lock()
+	vals := make([]string, len(keys))
+	for i, k := range keys {
+		vals[i] = m.M[k]
+	}
+	return vals
+}
+
+// HasSuffix returns a new StringMap containing only those entries that
+// have values with the specified suffix. HasSuffix is safe for
+// concurrency. See strings.HasSuffix.
+func (m StringMap) HasSuffix(s string) *StringMap {
+	n := NewStringMap()
+	defer n.Unlock()
+	n.Lock()
+	for k, v := range m.M {
+		if strings.HasSuffix(v, s) {
+			n.M[k] = v
+		}
+	}
+	return n
+}
+
+// HasPrefix returns a new StringMap containing only those entries that
+// have values with the specified prefis. HasPrefix is safe for
+// concurrency. See strings.HasPrefix.
+func (m StringMap) HasPrefix(s string) *StringMap {
+	n := NewStringMap()
+	defer n.Unlock()
+	n.Lock()
+	for k, v := range m.M {
+		if strings.HasPrefix(v, s) {
+			n.M[k] = v
+		}
+	}
+	return n
+}
+
+// ---------------------------- marshaling ----------------------------
+
+// JSON is shortcut for json.Marshal(m). See ToJSON.
+func (m StringMap) JSON() string { return ToJSON(m.M) }
+
+// String fullfills fmt.Stringer interface as JSON.
+func (m StringMap) String() string { return ToJSON(m.M) }
+
+// YAML is shortcut for MarshalYAML as string.
+func (m StringMap) YAML() string { return ToYAML(m.M) }
+
+// Print outputs as YAML (nice when testing).
+func (m StringMap) Print() { fmt.Print(ToYAML(m.M)) }
+
+// MarshalJSON implements the json.Marshaler interface using the
+// internal (M) map.
+func (m StringMap) MarshalJSON() ([]byte, error) { return json.Marshal(m.M) }
+
+// UnmarshalJSON implements the json.Unmarshaler interface using the
+// internal (M) map.
+func (m *StringMap) UnmarshalJSON(data []byte) error {
+	return json.Unmarshal(data, &m.M)
+}
+
+// MarshalYAML implements the yaml.Marshaler interface using the internal
+// (M) map.
+func (m StringMap) MarshalYAML() (interface{}, error) { return m.M, nil }
+
+// TODO
+// func (m *StringMap) UnmarshalYAML(node *yaml.Node) error {
+// return yaml.Unmarshal(node, &m.M)
+// }
+
+// Load opens and buffers the file at path and unmarshals it with
+// yaml.Unmarshal (which includes JSON) in a way that is safe for
+// concurrency.
+func (m *StringMap) Load(path string) error {
+	// TODO lock, load, and unmarshal
+	return fmt.Errorf("load not yet implemented")
+}
---
+++ util/stringmap_test.go
@@ -0,0 +1,283 @@
+/*
+Copyright 2021 Robert S. Muhlestein.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util_test
+
+import (
+	"fmt"
+
+	"github.com/rwxrob/cmdbox/util"
+	"gopkg.in/yaml.v3"
+)
+
+func ExampleStringMap() {
+	m := util.ToStringMap(map[string]interface{}{
+		"f":   "foo",
+		"foo": "foo",
+		"b":   "bar",
+		"bar": "bar",
+	})
+	fmt.Println(m.Same())
+	fmt.Println(m.Aliases())
+	fmt.Println(m.Keys())
+	fmt.Println(m.JSON())
+	fmt.Println(m)
+	fmt.Print(m.YAML())
+	m.Print()
+	// Output:
+	// [bar foo]
+	// [b f]
+	// [b bar f foo]
+	// {"b":"bar","bar":"bar","f":"foo","foo":"foo"}
+	// {"b":"bar","bar":"bar","f":"foo","foo":"foo"}
+	// b: bar
+	// bar: bar
+	// f: foo
+	// foo: foo
+	// b: bar
+	// bar: bar
+	// f: foo
+	// foo: foo
+}
+
+func ExampleNewStringMap() {
+	m := util.NewStringMap()
+	m.Print()
+	m.Set("foo", "bar")
+	m.Print()
+
+	// Output:
+	// {}
+	// foo: bar
+}
+
+func ExampleToStringMap() {
+	m := map[string]interface{}{
+		"foo": "a foo",
+		"bar": "a bar",
+	}
+	n := util.ToStringMap(m)
+	fmt.Printf("%T != %T\n", m, n)
+	n.Print()
+	// Output:
+	// map[string]interface {} != *util.StringMap
+	// bar: a bar
+	// foo: a foo
+}
+
+func ExampleStringMap_Init() {
+	m := util.NewStringMap()
+	m.Set("foo", "bar")
+	m.Print()
+	m.Init()
+	m.Print()
+
+	// Output:
+	// foo: bar
+	// {}
+}
+
+func ExampleStringMap_Get() {
+	m := util.NewStringMap()
+	fmt.Println(m.Get("foo"))
+	m.Set("foo", "fooval")
+	fmt.Println(m.Get("foo"))
+	fmt.Println(m.Get("bar"))
+
+	// Output:
+	//
+	// fooval
+
+}
+
+func ExampleStringMap_Set() {
+	m := util.NewStringMap()
+	fmt.Println(m.Get("foo"))
+	m.Set("foo", "bar")
+	fmt.Println(m.Get("foo"))
+
+	// Output:
+	//
+	// bar
+}
+
+func ExampleStringMap_Delete() {
+	m := util.NewStringMap()
+	m.Set("foo", "1")
+	m.Set("bar", "2")
+	m.Set("you", "3")
+	m.Print()
+	m.Delete("foo", "bar")
+	m.Print()
+
+	// Output:
+	// bar: "2"
+	// foo: "1"
+	// you: "3"
+	// you: "3"
+}
+
+func ExampleStringMap_Same() {
+	m := util.NewStringMap()
+	m.Set("foo", "1")
+	m.Set("bar", "bar")
+	m.Set("you", "you")
+	fmt.Println(m.Same())
+
+	// Output:
+	// [bar you]
+}
+
+func ExampleStringMap_Keys() {
+	m := util.NewStringMap()
+	m.Set("foo", "1")
+	m.Set("bar", "bar")
+	m.Set("you", "you")
+	fmt.Println(m.Keys())
+
+	// Output:
+	// [bar foo you]
+}
+
+func ExampleStringMap_Values() {
+	m := util.NewStringMap()
+	m.Set("foo", "1")
+	m.Set("bar", "bar")
+	m.Set("you", "1")
+	fmt.Println(m.Values())
+
+	// Output:
+	// [1 bar]
+}
+
+func ExampleStringMap_Aliases() {
+	m := util.NewStringMap()
+	m.Set("foo", "1")
+	m.Set("f", "1")
+	m.Set("bar", "bar")
+	m.Set("b", "bar")
+	m.Set("you", "1")
+	fmt.Println(m.Aliases())
+
+	// Output:
+	// [b f foo you]
+}
+
+func ExampleStringMap_Slice() {
+	m := util.NewStringMap()
+	m.Set("foo", "fooval")
+	m.Set("f", "fval")
+	m.Set("bar", "barval")
+	m.Set("b", "bval")
+	m.Set("you", "youval")
+	fmt.Println(m.Slice("f", "b", "none", "you"))
+
+	// Output:
+	// [fval bval  youval]
+}
+
+func ExampleStringMap_HasSuffix() {
+	m := util.NewStringMap()
+	m.Set("foo", "fooval")
+	m.Set("f", "fval")
+	m.Set("bar", "barval")
+	m.Set("b", "b")
+	m.Set("you", "you")
+	m.HasSuffix("val").Print()
+
+	// Output:
+	// bar: barval
+	// f: fval
+	// foo: fooval
+
+}
+
+func ExampleStringMap_HasPrefix() {
+	m := util.NewStringMap()
+	m.Set("foo", "fooval")
+	m.Set("f", "fval")
+	m.Set("bar", "barval")
+	m.Set("b", "b")
+	m.Set("you", "you")
+	m.HasPrefix("f").Print()
+
+	// Output:
+	// f: fval
+	// foo: fooval
+
+}
+func ExampleStringMap_JSON() {
+	m := util.NewStringMap()
+	m.Set("foo", "1")
+	m.Set("bar", "bar")
+	fmt.Println(m.JSON())
+
+	// Output:
+	// {"bar":"bar","foo":"1"}
+}
+
+func ExampleStringMap_String() {
+	m := util.NewStringMap()
+	m.Set("foo", "1")
+	m.Set("bar", "bar")
+	fmt.Println(m)
+
+	// Output:
+	// {"bar":"bar","foo":"1"}
+}
+
+func ExampleStringMap_YAML() {
+	m := util.NewStringMap()
+	m.Set("foo", "1")
+	m.Set("bar", "bar")
+	fmt.Println(m.YAML())
+
+	// Output:
+	// bar: bar
+	// foo: "1"
+}
+
+func ExampleStringMap_Print() {
+	m := util.NewStringMap()
+	m.Set("foo", "1")
+	m.Set("bar", "bar")
+	m.Print()
+
+	// Output:
+	// bar: bar
+	// foo: "1"
+}
+
+func ExampleStringMap_Rename() {
+	m := util.NewStringMap()
+	m.Set("foo", "val")
+	m.Print()
+	m.Rename("foo", "bar")
+	m.Print()
+
+	// Output:
+	// foo: val
+	// bar: val
+}
+
+func ExampleStringMap_MarshalYAML() {
+	m := util.NewStringMap()
+	m.Set("foo", "val")
+	byt, _ := yaml.Marshal(m)
+	fmt.Println(string(byt))
+	// Output:
+	// foo: val
+}
